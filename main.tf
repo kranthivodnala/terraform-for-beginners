37c67cb4338a71d556c4372ad0ef62dd7c578c31
thechroniclecloud <kranthi.vodnala1991@gmail.com>
2022-07-28 15:17:52 -0400

    create ec2 with security group

--- main.tf
+++ main.tf
@@ -5,21 +5,43 @@ provider "aws" {
 
 # create ec2 instance
 
-resource "aws_instance" "myfirsterraform" {
+resource "aws_instance" "my-first-ec2" {
     ami = "ami-0cff7528ff583bf9a"
+    count = 1
+    key_name = "abcd"
     instance_type = "t2.micro"
-
+    security_groups = ["security_ec2_port"]
     tags = {
-        Name = "TestServer"
+        Name = "my-first-ec2"
     }
-  
 }
 
-# create code commit repository
+# security group with ports 80 and 443
+resource "aws_security_group" "security_ec2_port" {
+    name = "security_ec2_port"
+    description = "This SG is to listen ports on 80 and 443"
 
-resource "aws_codecommit_repository" "wordpressapp" {
-  repository_name = "thechroniclecloud"
-  description = "Sample wordpress application"
-  default_branch = "dev"
-  
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+tags = {
+    Name = "security_ec2_port"
+}
 }
